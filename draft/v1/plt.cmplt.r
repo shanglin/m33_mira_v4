6a4af6b0aa28191a9f58931e7a289fd336a41b33
wl <wenlong@Pingmeis-MacBook-Pro.local>
2016-06-10 23:27:14 -0500

    .

---
+++ draft/v1/plt.cmplt.r
@@ -0,0 +1,100 @@
+dir = '~/Work/m33_phaseII/m33_ofiles/complete_fun/'
+outdir = '~/Work/m33_phaseII/draft/v1/figures/'
+f.eps = paste0(outdir,'lum_fun.eps')
+mag.shift = 6.2
+
+width=9
+height = width/1.618
+setEPS()
+postscript(f.eps, width=width, height=height)
+f.dat = '~/Work/m33_phaseII/m33_ofiles/m33i_bright.dat'
+## dat = read.table(f.dat)
+mag = dat[,4]
+mag.lim.1 = 18.5
+mag.lim.2 = 20
+mar1 = c(0,4.5,1,3)
+mar2 = c(4.5,4.5,0,3)
+mar3 = c(4.5,4.5,0,1)
+fig1 = c(0,0.55,0.56,1)
+fig2 = c(0,0.55,0,0.56)
+fig3 = c(0.45,1,0,1)
+hcol1 = '#dddddd'
+hcol2 = '#5555ee'
+
+par(mar=mar1, tck=0.04, fig=fig1)
+h = hist(mag, breaks=200, plot=F)
+x = h$mids
+y = h$counts
+ly = log10(y)
+xlim = c(18.5, 21.5)
+xlab = expression(paste(italic(I),' [mag]'))
+ylab = expression(paste('log (',italic(N),')'))
+plot(x, ly, xlim=xlim, ylim=c(1.8,4.8),type='s', xlab='', ylab=ylab, cex.lab=1.3, cex.axis=1.1, xaxt='n', yaxt='n')
+at = seq(18.5,21.5,0.5)
+label = rep('',length(at))
+axis(1, at = at, label=label)
+at = seq(2,5,0.5)
+label = as.character(at)
+axis(2, at=at, label=label)
+
+idx = which(x>mag.lim.1 & x<mag.lim.2)
+xg = x[idx]
+lyg = ly[idx]
+lines(xg, lyg, pch=19, col=4, type='s', lwd=2)
+
+n = length(xg)
+M = matrix(1, nrow=n, ncol=2)
+Y = matrix(lyg, nrow=n, ncol=1)
+M[,1] = xg
+fit = solve(t(M) %*% M) %*% (t(M) %*% Y)
+
+xc = c(0,max(mag))
+yc = fit[1]*xc + fit[2]
+lines(xc, yc, col=4, lty=2)
+
+ly.pred = fit[1]*x + fit[2]
+y.pred = 10^ly.pred
+
+par(mar=mar2, fig=fig2, new=T)
+ylab = expression(paste(italic(N) %*% '1e4'))
+plot(x, y.pred/1e4, type='l', col=4, xlim=xlim, xlab=xlab, ylab=ylab,
+     cex.lab=1.3, cex.axis=1.1, lty=2)
+lines(x, y/1e4, type='s')
+
+cpl = y/y.pred
+cpl[x<=mag.lim.2] = 1
+cpl = predict(smooth.spline(x,cpl))$y
+cpl[cpl>1] = 1
+scpl = cpl*50000
+
+lines(x, scpl/1e4, col=2)
+axis(4, at=seq(0,50000,10000)/1e4, labels=seq(0,1,0.2), col=2, col.axis=2)
+## dev.off()
+
+xx = seq(10, 30, by=0.001)
+yy = predict(smooth.spline(x,cpl), xx)$y
+yy[yy>1] = 1
+yy[xx>21.45] = 0
+com = cbind(xx, yy)
+com = round(com,3)
+## f.com = paste0(dir,'lum_fun.dat')
+## write.table(com, f.com, row.names=F, col.names=F, quote=F, sep='   ')
+
+
+f.csv = '~/Work/m33_phaseII/lmc_ofiles/mira.cat.csv'
+csv = read.csv(f.csv)
+mi = csv[,'I']
+mi = mi[mi>9]
+mi = mi + mag.shift
+h1 = hist(mi, plot=F, breaks=20)
+par(fig=fig3, new=T, tck=-0.02, mar=mar3)
+plot(h1, main='', yaxt='n', ylab='', col=hcol1, xlab=xlab, cex.lab=1.3, cex.axis=1.1)
+
+h2 = h1
+idx = match(h2$mids, com[,1])
+h2$counts = h2$counts * com[idx,2]
+plot(h2, add=T, col=hcol2)
+
+lines(com[,1],com[,2]*max(h1$counts)*0.8, col=2, lty=1, lwd=2)
+
+dev.off()
--- draft/v1/plt.simmira.r
+++ draft/v1/plt.simmira.r
@@ -1,13 +1,23 @@
 set.seed(101)
-dir = '~/Work/m33_phaseII/draft/figures/'
+dir = '~/Work/m33_phaseII/draft/v1/figures/'
 flcdir = '~/Work/m33_phaseII/sim_ofiles/flc_mira/'
 mlcdir = '~/Work/m33_phaseII/lmc_ofiles/mira.lcs/'
 slcdir = '~/Work/m33_phaseII/m33_ofiles/islcs/'
 
-fs.flc = list.files(flcdir)
-nfs = length(fs.flc)
-idx = sample(1:nfs,1)
-f.flc = fs.flc[idx]
+mag.shift = 6.2
+## fs.flc = list.files(flcdir)
+## nfs = length(fs.flc)
+## idx = sample(1:nfs,1)
+## f.flc = fs.flc[idx]
+f.flc = 'mira_42896_wki11760_204.74.flc'
+
+width = 10
+height = 8
+f.eps = paste0(dir, 'simmira.eps')
+setEPS()
+postscript(f.eps, width=width, height=height)
+
+par(mfrow=c(2,1), mar=c(2,5,3,1))
 
 mid = substr(f.flc, 6, 10)
 f.mlc = paste0(mlcdir, 'OGLE-LMC-LPV-',mid,'.dat')
@@ -28,14 +38,14 @@ lf.flc = paste0(flcdir, f.flc)
 
 mlc = read.table(f.mlc)
 x = mlc[,1]
-y = mlc[,2]
+y = mlc[,2] + mag.shift
 e = mlc[,3]
 xlab = 'HJD - 2450000'
-ylab = 'I [mag]'
+ylab = expression(paste(italic('I'),' + 6.2 [mag]'))
 main = ''
 xlim = c(min(x)-5, max(x)+5)
 ylim = c(max(y)+0.3, min(y)-0.3)
-plot(x, y, xlab=xlab, ylab=ylab, pch=19, cex=0.5, main=main, xlim=xlim, ylim=ylim)
+plot(x, y, xlab='', ylab=ylab, pch=19, cex=0.5, main=main, xlim=xlim, ylim=ylim, cex.axis=1.1, cex.lab=1.3)
 arrows(x, y+e, x, y-e, angle=90, code=3, length=0.001)
 
 f.lmc.cat = paste0('~/Work/m33_phaseII/lmc_ofiles/','mira','.cat.csv')
@@ -46,9 +56,45 @@ f.para = '~/Work/m33_phaseII/lmc_ofiles/allLMC.txt'
 f.basicfun = '~/Work/m33_phaseII/code.phaseii/GPcodes/r_v2/BasicFuns.R'
 library(VarStar)
 source(f.basicfun)
-paraAll = read.table(f.para, header=FALSE)
+paraAll = read.table(f.para, header=FALSE, stringsAsFactors = F)
 paraIDs = paraAll[,1]
 thetasAll = as.matrix(paraAll[,3:9])
 thetasAll = abs(thetasAll)
 thetasAll[,4] = 100000
 
+idx.para = paraIDs == paste0('OGLE-LMC-LPV-',mid)
+id.para = paraIDs[idx.para]
+ts.thetas = thetasAll[idx.para,]
+period = lmc.cat[lmc.cat[,1]==id.para,'P_1']
+x = new(gpModel, mlc[,1], mlc[,2], mlc[,3], mean(mlc[,2]))
+x$set_period(period,0)
+x$gp_setTheta(ts.thetas)
+xcnt = seq(min(mlc[,1]), max(mlc[,1]), 1)
+ycnt1 = x$gp_predict(xcnt,1)$predy
+ycnt3 = x$gp_predict(xcnt,3)$predy
+ycnt2 = x$gp_predict(xcnt,2)$predy
+ycnt = ycnt1+ycnt2+ycnt3-2*mean(mlc[,2]) + mag.shift
+lines(xcnt,ycnt,col=4)
+xs = c(min(mlc[,1]), min(mlc[,1]) + tshift)
+thisy = ylim[1] - 0.1*(ylim[1]-ylim[2])
+arrows(xs[1], thisy, xs[2], thisy, lty=1, lwd=2, col=4, code=3, length=0.05)
+thisy = ylim[1] - 0.05*(ylim[1]-ylim[2])
+text(mean(xs), thisy, expression(italic('s')), col=4, cex=1.3)
+
+par(mar=c(5,5,0,1))
+slc = read.table(f.slc)
+shifted.t = slc[,1] - min(slc[,1]) + min(mlc[,1]) + tshift
+abline(v = shifted.t, lwd=0.5)
+
+flc = read.table(lf.flc)
+x = flc[,1]
+y = flc[,2]
+e = flc[,3]
+x = x - min(x) + min(mlc[,1]) + tshift
+ylab = expression(paste(italic('I'),' [mag]'))
+plot(x, y, pch=19, xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab, cex=0.5,
+     cex.axis=1.1, cex.lab=1.3)
+arrows(x, y-e, x, y+e,
+       code=3, length=0.0, angle=90)
+lines(xcnt,ycnt,col=4)
+dev.off()
